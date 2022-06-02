02cf86c2993d23eb1def889a3608b0077c47b6a3
Nigel Yeo <nigelyyz@yahoo.com.sg>
2022-06-02 13:27:27 +0800

    Added record associations and fixed devise method error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_02_050124) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_051142) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -34,6 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_02_050124) do
     t.time "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_visits_on_user_id"
   end
 
 end
